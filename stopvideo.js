4575339cb5458b39671461374f4add852f5e2fac
avrambogdan00 <53436932+avrambogdan00@users.noreply.github.com>
2019-08-08 13:57:18 +0300

    Update stopvideo.js

--- stopvideo.js
+++ stopvideo.js
@@ -1,7 +1,7 @@
 function stopVideo() {
     var video = document.getElementById('video');
     video.pause();
-    setTimeout(function(){
-        video.play();
-    }, 2000);
+    //setTimeout(function(){
+    //    video.play();
+    //}, 2000);
 }
